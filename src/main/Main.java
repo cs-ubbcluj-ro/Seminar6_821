92505fda03bed4a8a260299cfb82bd4607c2408d
viktorashi <33178074+viktorashi@users.noreply.github.com>
2023-12-14 11:25:07 +0200

    seminar6

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
+import threads.Util;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] res =  Util.generateArray(10000);
+        System.out.println(Arrays.toString(res));
     }
+
 }
--- src/threads/Util.java
+++ src/threads/Util.java
@@ -1,4 +1,26 @@
 package threads;
 
+import java.util.Random;
+
 public class Util {
+    public static boolean isPrime(int n) {
+        if (n<2 || n % 2 ==0 ) return  false;
+        if (n==2 ) return true;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return  false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] generateArray(int arrayLength) {
+        Random rand = new Random();
+        int[] arr = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            arr[i]  =rand.nextInt(1000);
+        }
+        return arr;
+    }
+
 }
